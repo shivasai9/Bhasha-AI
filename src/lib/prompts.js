519e5ca8664307d352d57831f8092789fa48f275
KiranSaiB <kiran97chintu@gmail.com>
2024-11-11 10:17:58 +0530

    Added Quiz prompt

--- src/lib/prompts.js
+++ src/lib/prompts.js
@@ -17,3 +17,37 @@ Generate an educational article about the following topic: {{topic}}. Return it 
 }
 Please provide exactly five image keywords. Do not include any markdown or code formatting, only the pure JSON object.
 `;
+
+
+export const CREATE_ARTICLE_QUESTIONS = `
+Generate 5 questions from the article "{{title}}" with 4 options and answer. Return it in the following JSON format:
+[
+{"question":"question from the article",
+    "options": ["A. Option1", "B. Option2", "C. Option3", "D. Option4"],
+    "answer": "Answer for the question",
+    "explanation" : "1 line explanation about the answer",
+},
+{"question":"question from the article",
+    "options": ["A. Option1", "B. Option2", "C. Option3", "D. Option4"],
+    "answer": "Answer for the question",
+    "explanation" : "1 line explanation about the answer",
+},
+{"question":"question from the article",
+    "options": ["A. Option1", "B. Option2", "C. Option3", "D. Option4"],
+    "answer": "Answer for the question",
+    "explanation" : "1 line explanation about the answer",
+},
+{"question":"question from the article",
+    "options": ["A. Option1", "B. Option2", "C. Option3", "D. Option4"],
+    "answer": "Answer for the question",
+    "explanation" : "1 line explanation about the answer",
+},
+{"question":"question from the article",
+    "options": ["A. Option1", "B. Option2", "C. Option3", "D. Option4"],
+    "answer": "Answer for the question",
+    "explanation" : "1 line explanation about the answer",
+}
+]
+Please provide exactly five questions. Do not include any markdown or code formatting, only the pure array of JSON object.
+`;
+
